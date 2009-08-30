984b3f85d1945cce379f426516c4ed2f206725f8
Paul Collier <paul@paulcollier.ca>
2009-08-30 16:07:45 -0700

    Threaded env through hm; needs monad badly

--- hm.py
+++ hm.py
@@ -3,6 +3,8 @@ from atom import *
 from base import *
 from builtins import *
 
+Env = DT('Env', ('envTable', {Atom: Atom}))
+
 map(add_sym, 'type,void,int,bool,char,str,func,typevar'.split(','))
 
 voidT = lambda: symref('void', [])
@@ -12,75 +14,75 @@ charT = lambda: symref('char', [])
 strT = lambda: symref('str', [])
 fnT = lambda args, ret: symref('func', [Int(len(args)+1, [])] + args + [ret])
 
-def fresh():
+def fresh(env):
     return symref('typevar', [])
 
 basic_types = set(['void', 'int', 'bool', 'char', 'str'])
 
-def unify(e1, e2):
-    # TODO: env, substs
+def unify(e1, e2, env):
+    # TODO: substs
     ok = match((e1, e2),
             ("(key('typevar'), _)", lambda: True),
             ("(_, key('typevar'))", lambda: True),
             ("(key('func', sized(ss)), key('func', sized(ts)))",
-                lambda ss, ts: len(ss) == len(ts)
-                               and all(unify(s, t) for s, t in zip(ss, ts))),
+                lambda ss, ts: len(ss) == len(ts) and
+                               all(unify(s, t, env) for s, t in zip(ss, ts))),
             # These two must be last
             ("(key(s), key(t))", lambda s, t: s == t and s in basic_types),
             ("_", lambda: False))
     assert ok, "Could not unify %r with %r" % (e1, e2)
     return True
 
-def set_type(e, t):
-    e.subs.append(symref('type', [t]))
+def set_type(e, t, env):
+    env.envTable[e] = t
 
-def infer_call(f, args):
-    ft = infer_expr(f)
-    retT = fresh()
-    unify(ft, fnT(map(infer_expr, args), retT))
+def infer_call(f, args, env):
+    ft = infer_expr(f, env)
+    retT = fresh(env)
+    unify(ft, fnT([infer_expr(a, env) for a in args], retT), env)
     return retT
 
-def infer_builtin(k):
+def infer_builtin(k, env):
     if k == '+':
         return fnT([intT(), intT()], intT())
 
-def unknown_infer(a):
+def unknown_infer(a, env):
     assert False, 'Unknown type for:\n%s' % (a,)
 
-def infer_expr(a):
+def infer_expr(a, env):
     t = match(a,
         ("Int(_, _)", intT),
         ("Str(_, _)", strT),
-        ("key('call', cons(f, sized(args)))", infer_call),
-        ("key(k)", infer_builtin),
-        ("otherwise", unknown_infer))
-    set_type(a, t)
+        ("key('call', cons(f, sized(s)))", lambda f, s: infer_call(f, s, env)),
+        ("key(k)", lambda k: infer_builtin(k, env)),
+        ("otherwise", lambda e: unknown_infer(e, env)))
+    set_type(a, t, env)
     return t
 
-def infer_DT(fs, nm):
+def infer_DT(fs, nm, env):
     print 'DT', nm
 
-def infer_assign(a, e):
+def infer_assign(a, e, env):
     newvar = match(a, ("key('var')", lambda: True), ("_", lambda: False))
     assert newvar
-    t = fresh()
-    unify(t, infer_expr(e))
-    set_type(a, t)
+    t = fresh(env)
+    unify(t, infer_expr(e, env), env)
+    set_type(a, t, env)
 
-def infer_exprstmt(e):
-    infer_expr(e) # discard type
-
-def infer_stmt(a):
+def infer_stmt(a, env):
     match(a,
         ("key('DT', all(fs, key('field') and named(fnm)))"
-            " and named(nm)", infer_DT),
-        ("key('=', cons(a, cons(e, _)))", infer_assign),
-        ("key('exprstmt', cons(e, _))", infer_exprstmt),
-        ("otherwise", unknown_infer))
+            " and named(nm)", lambda fs, nm: infer_DT(fs, nm, env)),
+        ("key('=', cons(a, cons(e, _)))", lambda a, e: infer_assign(a,e,env)),
+        ("key('exprstmt', cons(e, _))", lambda e: infer_expr(e, env)),
+        ("otherwise", lambda e: unknown_infer(e, env)))
 
 def infer_types(roots):
+    env = Env({})
     for r in roots:
-        infer_stmt(r)
+        infer_stmt(r, env)
+    for a, t in env.envTable.iteritems():
+        a.subs.append(symref('type', [t]))
 
 if __name__ == '__main__':
     import ast
