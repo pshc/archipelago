5b40654a734dfe2f04d729b6c35552b411a6d69f
Paul Collier <paul@paulcollier.ca>
2009-10-02 13:07:02 -0400

    Add envs and Str generation for c.py

--- c.py
+++ c.py
@@ -3,40 +3,90 @@ from base import *
 from atom import *
 from builtins import *
 
+CEnv = DT('CEnv', ('cenvIndent', int),
+                  ('cenvStmts', [Atom]),
+                  ('cenvOuterEnv', 'CEnv'))
+# The final version of this will thread this state through the functions
+# using the type system.
+CENV = None
+
+def str_(st):
+    return Str(st, [])
+def stmt(s):
+    global CENV
+    list_append(CENV.cenvStmts, s)
+def indent(s):
+    global CENV
+    i = CENV.cenvIndent
+    if i == 0:
+        stmt(cat(s, '\n'))
+    else:
+        stmt(Str('  ' * i, [s, str_('\n')]))
+    return None
+def blank_line():
+    stmt(str_('\n'))
+
+def bracket(before, c, after):
+    assert isinstance(before, basestring)
+    assert isinstance(after, basestring)
+    return Str(before, [c, str_(after)])
+def brackets(before, ss, after):
+    assert isinstance(before, basestring)
+    assert isinstance(after, basestring)
+    return Str(before, ss + [str_(after)])
+def cat(a, after):
+    assert isinstance(after, basestring)
+    list_append(a.subs, str_(after))
+    return a
+def cats(a, ss):
+    assert isinstance(ss, list)
+    a.subs += ss
+    return a
+def semi(): return str_(';')
+def comma(): return str_(', ')
+def commas(ss):
+    assert isinstance(ss, list)
+    n = len(ss)
+    for s in ss:
+        n -= 1
+        if n > 0:
+            cat(s, ', ')
+    return ss
+
 def c_type(ta):
     t = atoms_to_scheme(ta)
-    return match(t.schemeType,
+    return str_(match(t.schemeType,
         ("TInt()", lambda: 'int'),
         ("TStr()", lambda: 'char *'),
         ("TTuple(_)", lambda: 'struct tuple'),
         ("TNullable()", lambda: 'void *'),
         ("TVar(_)", lambda: 'void *'),
-        ("TVoid()", lambda: 'void'))
+        ("TVoid()", lambda: 'void')))
 
 def c_defref(nm, ss):
     return match(ss,
-        ("contains(key('unaryop', cons(Str(s, _), _)))",
-            lambda s: '%s%%s' % (s,)),
-        ("contains(key('binaryop', cons(Str(s, _), _)))",
-            lambda s: '%%s %s %%s' % (s.replace('%', '%%'),)),
-        ("contains(key('crepr', cons(Str(s, _), _)))", identity),
-        ("_", lambda: nm))
+        ("contains(k==key('unaryop' or 'binaryop' or 'crepr'))", identity),
+        ("_", lambda: str_(nm)))
 
 def c_call(f, args):
     cf = c_expr(f)
-    cargs = map(c_expr, args)
-    if cf.startswith('%'): # op
-        return cf % tuple(cargs)
-    else:
-        return '%s(%s)' % (cf, ', '.join(cargs))
+    cargs = [c_expr(a) for a in args]
+    return match(cf,
+        ("key('unaryop', cons(s, _))",
+            lambda s: cats(s, cargs)),
+        ("key('binaryop', cons(Str(s, _), _))",
+            lambda s: cats(cargs[0], [str_(' %s ' % (s,)), cargs[1]])),
+        ("key('crepr', cons(s, _))",
+            lambda s: cats(s, [brackets('(', commas(cargs), ')')])),
+        ("_", lambda: cats(cf, [brackets('(', commas(cargs), ')')])))
 
 def c_tuple(ts):
-    return '{%s}' % (', '.join(c_expr(t) for t in ts),)
+    return brackets('{', commas(map(c_expr, ts)), '}')
 
 def c_expr(e):
     return match(e,
-        ("Int(i, _)", lambda i: "%d" % (i,)),
-        ("Str(s, _)", lambda s: escape_str(s)),
+        ("Int(i, _)", lambda i: str_("%d" % (i,))),
+        ("Str(s, _)", lambda s: str_(escape_str(s))),
         ("Ref(named(nm, ss==contains(key('type'))), _, _)", c_defref),
         ("key('call', cons(f, sized(args)))", c_call),
         ("key('tuplelit', sized(ts))", c_tuple))
@@ -45,64 +95,75 @@ def c_assign(a, e):
     ce = c_expr(e)
     ca = match(a,
         ("key('var', contains(t==key('type'))) and named(nm)",
-            lambda t, nm: '%s %s' % (c_type(t), nm)),
-        ("Ref(named(nm, contains(key('type'))), _, _)", identity))
-    return '%s = %s;' % (ca, ce)
+            lambda t, nm: cat(c_type(t), ' %s' % nm)),
+        ("Ref(named(nm, contains(key('type'))), _, _)", str_))
+    indent(cats(ca, [str_(' = '), ce, semi()]))
 
 def c_cond(cs):
     first = True
-    ss = []
     for (t, b) in cs:
-        s = ''
         if match(t, ("key('else')", lambda: False), ("_", lambda: True)):
-            s = '%s (%s) {\n' % ('if' if first else '\nelse if', c_expr(t))
+            kw = 'if' if first else 'else if'
+            indent(Str(kw, [str_(' ('), c_expr(t), str_(') {')]))
             first = False
         else:
             assert not first
-            s = '\nelse {\n'
-        list_append(ss, s)
-        ss += c_body(b)
-        list_append(ss, '}')
-    return ''.join(ss)
+            indent(str_('else {'))
+        c_body(b)
+        indent(str_('}'))
 
 def c_assert(t, m):
-    return "assert(%s, %s);" % (c_expr(t), c_expr(m))
+    indent(Str("assert(", [c_expr(t), comma(), c_expr(m), str_(');')]))
 
 def c_args(args):
-    return ', '.join(match(a,
-        ("named(nm, contains(t==key('type')))",
-            lambda nm, t: "%s %s" % (c_type(t), nm))) for a in args)
+    return commas([match(a, ("named(nm, contains(t==key('type')))",
+                         lambda nm, t: cat(c_type(t), ' %s' % (nm,))))
+                   for a in args])
 
 def c_func(t, args, body, nm):
     # Wow this is bad
     t_ = atoms_to_scheme(t).schemeType
     retT = c_type(scheme_to_atoms(Scheme([], t_.funcRet)))
-    c = ['%s %s(%s) {\n' % (retT, nm, c_args(args))]
-    c += c_body(body)
-    list_append(c, '}\n')
-    return ''.join(c)
+    blank_line()
+    indent(Str('', [retT, str_(' %s(' % (nm,))]
+                          + c_args(args) + [str_(') {')]))
+    c_body(body)
+    indent(str_('}'))
 
 def c_stmt(s):
-    return match(s,
-        ("key('exprstmt', cons(e, _))", lambda e: '%s;' % (c_expr(e),)),
+    match(s,
+        ("key('exprstmt', cons(e, _))",
+            lambda e: indent(cat(c_expr(e), ';'))),
         ("key('=', cons(a, cons(e, _)))", c_assign),
         ("key('cond', all(cs, key('case', cons(t, sized(b)))))", c_cond),
         ("key('assert', cons(t, cons(m, _)))", c_assert),
-        ("key('DT') and named(nm)", lambda nm: "struct %s {};" % (nm,)),
+        ("key('DT') and named(nm)",
+            lambda nm: indent(str_("struct %s {};" % (nm,)))),
         ("key('func', contains(t==key('type')) "
                  "and contains(key('args', sized(a))) "
                  "and contains(key('body', sized(b)))) and named(nm)", c_func),
-        ("key('return', cons(e, _))", lambda e: "return %s;" % (c_expr(e),)),
-        ("key('returnnothing')", lambda: "return;"))
+        ("key('return', cons(e, _))",
+            lambda e: indent(bracket("return ", c_expr(e), ';'))),
+        ("key('returnnothing')", lambda: indent(str_("return;"))))
 
 def c_body(ss):
-    return ['%s\n' % c_stmt(s) for s in ss]
+    global CENV
+    corig = CENV
+    CENV = CEnv(corig.cenvIndent + 1, [], corig)
+    for s in ss:
+        c_stmt(s)
+    assert corig is CENV.cenvOuterEnv
+    corig.cenvStmts += CENV.cenvStmts
+    CENV = corig
 
 def generate_c(mod):
-    rs = [Str(s, []) for s in c_body(mod.roots)]
-    return Module("c_" + mod.name, None, rs)
+    global CENV
+    CENV = CEnv(-1, [], None)
+    c_body(mod.roots)
+    return Module("c_" + mod.name, None, CENV.cenvStmts)
 
 def _write_c_strs(f, atom):
+    assert isinstance(atom, Str), "Need Str, got: %s" % (atom,)
     fwrite(f, atom.strVal)
     for sub in atom.subs:
         _write_c_strs(f, sub)
