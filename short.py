eb217a04bb516d16f7995db250e10e74ee1f845c
Paul Collier <paul@paulcollier.ca>
2009-12-14 02:42:25 -0500

    Move short.py statements into main()

--- short.py
+++ short.py
@@ -4,23 +4,25 @@ def ok(n):
     return s
 def ident(val):
     return val
-while False:
-    inside = 1
-a = 1 + 2
-ok(a)
-if True:
-    print 'ok'
-elif False:
-    print 'what'
-else:
-    print 'no'
-assert True, 'WHAT'
-t = (1, 2)
-c = None
-c = 'ok'
-Pair = DT('Pair', ('first', int), ('second', int))
-Maybe, Just, Nothing = ADT('Maybe', 'Just', ('just', 'a'), 'Nothing')
-m = Just(1)
-match(m, ("Just(n)", identity), ("Nothing()", lambda: 0))
+def main():
+    while False:
+        inside = 1
+    a = 1 + 2
+    ok(a)
+    if True:
+        print 'ok'
+    elif False:
+        print 'what'
+    else:
+        print 'no'
+    assert True, 'WHAT'
+    t = (1, 2)
+    c = None
+    c = 'ok'
+    Pair = DT('Pair', ('first', int), ('second', int))
+    Maybe, Just, Nothing = ADT('Maybe', 'Just', ('just', 'a'), 'Nothing')
+    m = Just(1)
+    match(m, ("Just(n)", identity), ("Nothing()", lambda: 0))
+    return 0
 
 # vi: set sw=4 ts=4 sts=4 tw=79 ai et nocindent:
