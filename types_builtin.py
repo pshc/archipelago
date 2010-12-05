833f146e4e5e30bd3cfbac2c3ee7a238f237aff5
Paul Collier <paul@paulcollier.ca>
2010-12-05 01:34:44 -0800

    repr() for types and schemes

--- types_builtin.py
+++ types_builtin.py
@@ -15,6 +15,32 @@ Type, TVar, TMeta, TInt, TStr, TChar, TBool, TVoid, TNullable, \
         'TData', ('dataAtom', 'Atom'),
         'TApply', ('appType', 'Type'), ('appVars', ['Type']))
 
+def _get_name(a):
+    return match(a, ("named(nm)", lambda nm: nm))
+
+def _type_repr(t):
+    return match(t, ("TVar(a)", _get_name),
+                    ("TMeta(TypeCell(t))", _type_repr),
+                    ("TInt()", lambda: 'int'),
+                    ("TStr()", lambda: 'str'),
+                    ("TChar()", lambda: 'char'),
+                    ("TBool()", lambda: 'bool'),
+                    ("TVoid()", lambda: 'void'),
+                    ("TNullable(t)", lambda t: 'null(' + _type_repr(t) + ')'),
+                    ("TTuple(ts)", lambda ts: '(%s)' % ', '.join(_type_repr(t)
+                                                                 for t in ts)),
+                    ("TAnyTuple()", lambda: 'tuple(*)'),
+                    ("TFunc(s, r)", lambda s, r: ' -> '.join(_type_repr(t)
+                                                        for t in s + [r])),
+                    ("TData(d)", _get_name), #lambda d: 'data@0x%x' % id(d)),
+                    ("TApply(t, vs)", lambda t, vs: '%s%s' % (_type_repr(t),
+                                '.'.join(map(_type_repr, vs)))))
+
+_temp = locals().copy()
+for _t in _temp:
+    if _t[0:1] == 'T':
+        _temp[_t].__repr__ = _type_repr
+
 TypeCell = DT('TypeCell', ('cellType', Type))
 
 def map_type_vars(f, t):
@@ -32,6 +58,14 @@ def map_type_vars(f, t):
                     ("_", lambda: t))
 
 Scheme = DT('Scheme', ('schemeVars', [Type]), ('schemeType', Type))
+
+def _scheme_repr(s):
+    begin = ':: '
+    vs = s.schemeVars
+    if vs:
+        begin += ', '.join(map(_get_name, vs)) + ' => '
+    return begin + repr(s.schemeType)
+Scheme.__repr__ = _scheme_repr
 
 # TODO
 TDict = None
