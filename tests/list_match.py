f845fcfc145edd2ef55df3275971f5c940a61bb4
Paul Collier <paul@paulcollier.ca>
2012-06-21 00:01:53 +0800

    Disable match() test for now

--- tests/list_match.py
+++ tests/list_match.py
@@ -3,8 +3,8 @@ from bedrock import *
 @annot('void -> int')
 def main():
     a = hint(Cons(0, Cons(1, Nil())), a='int')
-    a = Cons(1, Cons(2, Cons(3, Nil)))
-    b = match(a, ("Cons(_, Cons(two, Cons(_, Nil())))", identity),
-                 ("_", lambda: 4))
-    assert b == 2, "List pattern match"
+    a = hint(Cons(1, Cons(2, Cons(3, Nil))), a='int')
+    #b = hint(match(a, ("Cons(_, Cons(two, Cons(_, Nil())))", identity),
+    #             ("_", lambda: 4)), a='int')
+    #assert b == 2, "List pattern match"
     return 0
