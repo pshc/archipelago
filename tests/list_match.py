e00140c1488fd17f44932dee3eb320e2ae697b90
Paul Collier <paul@paulcollier.ca>
2012-06-22 15:26:01 +0800

    Fix actual type error in test code

--- tests/list_match.py
+++ tests/list_match.py
@@ -3,7 +3,7 @@ from bedrock import *
 @annot('void -> int')
 def main():
     a = hint(Cons(0, Cons(1, Nil())), a='int')
-    a = hint(Cons(1, Cons(2, Cons(3, Nil))), a='int')
+    a = hint(Cons(1, Cons(2, Cons(3, Nil()))), a='int')
     #b = hint(match(a, ("Cons(_, Cons(two, Cons(_, Nil())))", identity),
     #             ("_", lambda: 4)), a='int')
     #assert b == 2, "List pattern match"
