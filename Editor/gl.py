b3fb3edfd25c7e6fd7dc896d25ae05fd8f78d1c9
Paul Collier <paul@paulcollier.ca>
2012-07-27 18:34:45 -0700

    Generate gl.py using importer

--- Editor/gl.py
+++
@@ -1,7 +0,0 @@
-glClear = cdecl('glClear', 'int -> void')
-glClearColor = cdecl('glClearColor', '(float, float, float, float) -> void')
-glUniformMatrix4fv = cdecl('glUniformMatrix4fv',
-        '(int, int, bool, [float]) -> void')
-
-GL_COLOR_BUFFER_BIT = 16384
-
