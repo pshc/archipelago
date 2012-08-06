f5a6f58c055e2835ec775a2a1049a6d92dc1643f
Paul Collier <paul@paulcollier.ca>
2012-08-05 23:38:15 -0700

    Importer specification

--- importer/import.py
+++ importer/import.py
@@ -3,14 +3,16 @@ import io
 import re
 import subprocess
 
-def macro_values(input, prefix):
+def macro_values(input, spec):
     c = subprocess.check_output(['clang', '-E', '-dM', input])
     for line in c.splitlines():
-        m = re.match(r'^#define\s+('+prefix+r'\w+)\s+([\da-fA-Fx]+)$', line)
+        m = re.match(r'^#define\s+(\w+)\s+([\da-fA-Fx]+)$', line)
         if m:
-            print m.group(1), '=', m.group(2)
+            name, val = m.groups()
+            if name in spec.consts:
+                print name, '=', m.group(2)
 
-def func_decls(input, prefix):
+def func_decls(input, spec):
     c = subprocess.check_output(['clang', '-cc1', '-ast-print', input])
     typedefs = {}
     for line in c.splitlines():
@@ -23,40 +25,36 @@ def func_decls(input, prefix):
             continue
         m = re.match(r'^extern\s+(.*)', line)
         if m:
-            parse_func_decl(list(tokenize(m.group(1))), typedefs, prefix)
+            parse_func_decl(list(tokenize(m.group(1))), typedefs, spec)
 
-def parse_func_decl(s, typedefs, prefix):
+def parse_func_decl(s, typedefs, spec):
     line = ' '.join(s)
     tvars = []
     retT = consume_type(s, typedefs, tvars)
     name = s.pop(0)
     assert s[0] == '(', "Couldn't parse retT of "+line+" at: " + ' '.join(s)
     s.pop(0)
-    if not name.startswith(prefix):
+    if name in spec.overrides:
+        typedesc = spec.overrides[name]
+    elif name not in spec.funcs:
         return
-    if 'Sync' in name: # XXX skip structs
-        return
-    argTs = []
-    while s[0] != ')':
-        argTs.append(consume_type(s, typedefs, tvars))
-        if s[0] not in ',)':
-            argNm = s.pop(0)
-        if s[0] == ',':
-            s.pop(0)
-        else:
-            assert s[0] == ')', "Expected comma, got " + ' '.join(s)
-    assert s == [')', ';'], 'Unexpected trailer: ' + ' '.join(s)
-
-    # XXX ignore variants for now
-    for t in argTs:
-        if 'double' in t or 'short' in t or 'char' in t or 'longlong' in t:
-            return
-
-    if len(argTs) > 1:
-        typedesc = '(%s) -> %s' % (', '.join(argTs), retT)
     else:
-        arg = argTs[0] if argTs else 'void'
-        typedesc = '%s -> %s' % (arg, retT)
+        argTs = []
+        while s[0] != ')':
+            argTs.append(consume_type(s, typedefs, tvars))
+            if s[0] not in ',)':
+                argNm = s.pop(0)
+            if s[0] == ',':
+                s.pop(0)
+            else:
+                assert s[0] == ')', "Expected comma, got " + ' '.join(s)
+        assert s == [')', ';'], 'Unexpected trailer: ' + ' '.join(s)
+
+        if len(argTs) > 1:
+            typedesc = '(%s) -> %s' % (', '.join(argTs), retT)
+        else:
+            arg = argTs[0] if argTs else 'void'
+            typedesc = '%s -> %s' % (arg, retT)
     decl = '%s = cdecl(%r, %r)' % (name, name, typedesc)
     if len(decl) > 79:
         decl = '%s = cdecl(%r,\n\t%r)' % (name, name, typedesc)
@@ -125,8 +123,22 @@ def tokenize(s):
     if word:
         yield word
 
+def load_spec(name):
+    spec = __import__(name)
+    spec.funcs = re.findall(r'\w+', spec.funcs)
+    spec.consts = re.findall(r'\w+', spec.consts)
+    def parse(line):
+        return re.match(r'^\s*(\w+)\s*::\s*(.*?)\s*$', line).groups()
+    lines = filter(None, spec.overrides.splitlines())
+    spec.overrides = dict(map(parse, lines))
+    return spec
+
 if __name__ == '__main__':
-    func_decls('gl.c', 'gl')
-    macro_values('gl.c', 'GL_')
+    import inspect, os.path, sys
+    spec = load_spec('spec')
+    path = os.path.dirname(inspect.getfile(inspect.currentframe()))
+    gl_c = os.path.join(path, 'gl.c')
+    func_decls(gl_c, spec)
+    macro_values(gl_c, spec)
 
 # vi: set sw=4 ts=4 sts=4 tw=79 ai et nocindent:
---
+++ importer/spec.py
@@ -0,0 +1,19 @@
+funcs = """
+glClear
+glClearColor
+glCreateShader
+glCompileShader
+glDeleteShader
+glGetShaderiv
+glShaderSource
+glUniformMatrix4fv
+"""
+
+consts = """
+GL_COLOR_BUFFER_BIT
+GL_COMPILE_STATUS
+GL_INFO_LOG_LENGTH
+"""
+
+overrides = """
+"""
