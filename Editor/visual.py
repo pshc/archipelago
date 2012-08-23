bea256a6d654a8f83890570aca43d33b27cf892b
Paul Collier <paul@paulcollier.ca>
2012-08-23 15:19:06 -0700

    Some more (broken) shader code

--- Editor/visual.py
+++ Editor/visual.py
@@ -1,6 +1,34 @@
 from Editor.gl import *
 from bedrock import *
 
+fragmentShaderSrc = """
+varying lowp vec2 uv;
+uniform sampler2D atlas;
+
+void main () {
+    lowp vec4 col = texture2D(atlas, uv);
+    gl_FragColor = vec4(uv, 1.0 - uv.x, col.a * (uv.y * 0.5 + 0.5));
+}
+"""
+
+vertexShaderSrc = """
+attribute lowp vec2 position;
+attribute lowp vec2 texCoord0;
+
+varying lowp vec2 uv;
+
+uniform mat4 modelViewMatrix;
+uniform mat4 projectionMatrix;
+uniform lowp vec2 invAtlasSize;
+
+void main() {
+    uv = texCoord0 * invAtlasSize;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position,0.0,1.0);
+}
+"""
+
+ATTRIB_POS = 0
+
 Vec2 = DT('Vec2', ('x', float), ('y', float))
 
 SceneNode = DT('SceneNode',
@@ -53,5 +81,45 @@ def compile_shader(src, kind):
         return 0
 
     return shader
+
+@annot('int -> bool')
+def link_program(program):
+    glLinkProgram(program)
+
+    logLen = [0]
+    glGetProgramiv(program, GL_INFO_LOG_LENGTH, logLen)
+    # check log
+
+    status = [0]
+    glGetProgramiv(program, GL_LINK_STATUS, status)
+    return True # status[0] != 0
+
+@annot('void -> int')
+def load_shader():
+    vertShader = compile_shader(vertexShaderSrc, GL_VERTEX_SHADER)
+    if vertShader == 0:
+        return 0
+    fragShader = compile_shader(fragmentShaderSrc, GL_FRAGMENT_SHADER)
+    if fragShader == 0:
+        glDeleteShader(vertShader)
+        return 0
+
+    program = glCreateProgram()
+    glAttachShader(program, vertShader)
+    glAttachShader(program, fragShader)
+
+    glBindAttribLocation(program, ATTRIB_POS, "position")
+
+    linked = link_program(program)
+    if linked:
+        pass # get uniforms
+    glDetachShader(program, vertShader)
+    glDetachShader(program, fragShader)
+    glDeleteShader(vertShader)
+    glDeleteShader(fragShader)
+    if not linked:
+        glDeleteProgram(program)
+        program = 0
+    return program
 
 # vi: set sw=4 ts=4 sts=4 tw=79 ai et nocindent:
--- importer/spec.py
+++ importer/spec.py
@@ -1,9 +1,16 @@
 funcs = """
+glAttachShader
+glBindAttribLocation
 glClear
 glClearColor
+glCreateProgram
 glCreateShader
 glCompileShader
+glDeleteProgram
 glDeleteShader
+glDetachShader
+glLinkProgram
+glGetProgramiv
 glGetShaderiv
 glShaderSource
 glUniformMatrix4fv
@@ -12,7 +19,10 @@ glUniformMatrix4fv
 consts = """
 GL_COLOR_BUFFER_BIT
 GL_COMPILE_STATUS
+GL_FRAGMENT_SHADER
 GL_INFO_LOG_LENGTH
+GL_LINK_STATUS
+GL_VERTEX_SHADER
 """
 
 overrides = """
