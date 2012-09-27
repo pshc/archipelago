ff1f37ce67829cafacc802bb139582d358c74366
Paul Collier <paul@paulcollier.ca>
2012-09-27 15:41:56 -0700

    Don't use invalid Just(int) in array hack

--- tests/array.py
+++ tests/array.py
@@ -6,7 +6,7 @@ def main():
     assert len(empty) == 0, "Empty length"
     strs = ["hello", "world"]
     assert len(strs) == 2, "Pointer array length"
-    dummy = fromJust(Just(0)) # TEMP hack for missing bedrock dep
+    dummy = fromJust(Just("dummy")) # TEMP hack for missing bedrock dep
     nothing = [Nothing(), Nothing(), Nothing()]
     assert len(nothing) == 3, "Array of nulls length"
     rets = [-1, -1, 0, -1]
