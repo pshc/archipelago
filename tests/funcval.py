aa2efc5f2f2218ed27dbd7ecfbb0dcdb46686de7
Paul Collier <paul@paulcollier.ca>
2012-10-06 14:57:56 -0700

    A little more funcval testing
    Not nearly sufficient though.

--- tests/funcval.py
+++ tests/funcval.py
@@ -8,6 +8,15 @@ def apply_two(f):
     g = f
     return g(2)
 
+@annot('void -> void')
+def make_multiple_func_vals():
+    # Sanity check ctx bindings
+    # XXX: test actual env operations, not just compilation success
+    f1 = triple
+    f2 = triple
+    assert f1(1) + f2(2) == 9, "Ctx-bound func vals"
+
 def main():
     assert apply_two(triple) == 6, "Function object"
+    make_multiple_func_vals()
     return 0
