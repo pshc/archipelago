2761ca23e1b0160b1504c8be4080a275e68fe459
Paul Collier <paul@paulcollier.ca>
2013-01-23 13:45:07 -0800

    Make list_match test non-completely-trivial to GC

--- tests/list_match.py
+++ tests/list_match.py
@@ -1,9 +1,15 @@
 from bedrock import *
 
-def main():
-    a = Cons(0, Cons(1, Nil()))
+@annot('void -> List(int)')
+def test():
+    kept = Cons(0, Cons(1, Nil()))
     a = Cons(1, Cons(2, Cons(3, Nil())))
     b = match(a, ("Cons(_, Cons(two, Cons(_, Nil())))", identity),
                  ("_", lambda: 4))
     assert b == 2, "List pattern match"
+    return kept
+
+def main():
+    kept = test()
+    nil = Nil()
     return 0
